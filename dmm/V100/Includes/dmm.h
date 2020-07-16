127ecb285c83a72253e1eade87a106cc09567201
Faiz Ur Reh,an <faizurrehman5743@gmail.com>
2020-07-15 21:41:41 -0400

    Released dmm V100

---
+++ dmm/V100/Includes/dmm.h
@@ -0,0 +1,65 @@
+/**
+ * @file dmm.h
+ * @author Faiz Ur Rehman
+ * @brief Dynamic memory management library
+ * @version 0.0
+ * @date 2020-07-04
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+
+
+#ifndef DMM_H
+#define DMM_H
+
+/*	header files **************************************************************/
+
+#include <inttypes.h>
+
+/*	External defines **********************************************************/
+/*	External macros ***********************************************************/
+/*	External enums ************************************************************/
+
+typedef enum
+{
+	eSuccess,
+	eMallocFail,
+	eReallocFail,
+    eOutOfMemory,
+    eInvalidArgument
+}
+eDmmError_t;
+
+/*	External structs **********************************************************/
+/*	External variables ********************************************************/
+/*	External function prototypes **********************************************/
+
+/** @brief allocate memory
+ *
+ * @param	ppPtr: address of the pointer to allocate memory
+ * @param	ulSize: size of memory
+ * @return	eDmmError_t: error status
+ *
+ */
+eDmmError_t eDmmMalloc( void** ppPtr, uint32_t ulSize );
+
+/** @brief re-allocate memory
+ *
+ * @param	ppPtr: address of the pointer to reallocate memory
+ * @param	ulSize: size of memory
+ * @return	eDmmError_t: error status
+ *
+ */
+eDmmError_t eDmmRealloc( void** ppPtr, uint32_t ulSize );
+
+
+/** @brief free allocated memory
+ *
+ * @param	ppPtr: address of the pointer to free memory
+ * @return	eDmmError_t: error status
+ *
+ */
+eDmmError_t eDmmFree( void** ppPtr );
+
+#endif
